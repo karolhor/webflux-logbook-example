1f0234e54bcdd8c2647b9574d387e8b64ae67810
Karol Horowski <karol.hor@gmail.com>
2024-10-02 01:27:06 +0200

    simplify config for logbook in application.yml

--- src/main/java/com/github/karolhor/example/ExampleController.java
+++ src/main/java/com/github/karolhor/example/ExampleController.java
@@ -1,5 +1,7 @@
 package com.github.karolhor.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class ExampleController {
+    private final Logger logger = LoggerFactory.getLogger(ExampleController.class);
 
     @GetMapping("/example")
     Mono<Map<String, String>> exampleEndpoint() {
+        logger.trace("Example endpoint log works");
         return Mono.just(Map.of("status", "ok"));
     }
 }
